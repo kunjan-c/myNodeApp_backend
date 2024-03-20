2054bd8b6904d824164d8b5e4e18de889711024f
Kunjan <choudharykunjan@gmail.com>
2024-03-21 01:56:12 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async () => {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // On this new page:
+  // - open the "http://quotes.toscrape.com/" website
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto("https://www.dolarhoy.com", {
+    waitUntil: "domcontentloaded",
+  });
+
+  // Get page data
+  const quotes = await page.evaluate(() => {
+    // Fetch the first element with class "quote"
+    const quote = document.querySelector(".values");
+
+    // Fetch the sub-elements from the previously fetched quote element
+    // Get the displayed text and return it (`.innerText`)
+    const compratext = quote.querySelector(".compra").innerText;
+    const ventatext = quote.querySelector(".venta").innerText;
+
+    return { compratext, ventatext };
+  });
+  
+
+  // Display the quotes
+  console.log(quotes);
+
+  // Close the browser
+  await browser.close();
+};
+
+// Start the scraping
+getQuotes();
